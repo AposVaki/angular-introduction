17293710b03e1274c8b0ce5d045550f42c9b3211
AposVaki <153004990+AposVaki@users.noreply.github.com>
2024-04-01 18:43:55 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,18 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Apostolos';
-
+  person0: Person = {
+    givenName: "Apostolos",
+    surName: "Vakirtzoglou",
+    age: 0x1e,    
+    email: "avakirtzoglou@gmail.com",
+    address: "Athens, Greece"
+  }
+  person1: Person = {
+    givenName: "Alice",
+    surName: "Wonder",
+    age: 0x1a,    
+    email: "alicew@gmail.com",
+    address: "Patras, Greece"
+  }
 }
