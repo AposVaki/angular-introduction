b6b4ee36f20cee0d8c9611020ff63e2f93fb9306
AposVaki <153004990+AposVaki@users.noreply.github.com>
2024-04-02 19:21:39 +0300

    fancy app menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   imports: [
     RouterLink, 
     RouterOutlet,
-    PersonTableComponent, 
-    EventBindExampleComponent, 
-    WelcomeComponent],
+    ListGroupMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
